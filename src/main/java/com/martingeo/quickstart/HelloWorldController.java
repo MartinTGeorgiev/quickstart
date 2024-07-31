e71f204fef07c11afee38b651e137770b1be9862
Martin Georgiev <martin.tonin@gmail.com>
2024-07-31 22:42:53 +0100

    Update HelloWorldController.java

--- src/main/java/com/martingeo/quickstart/HelloWorldController.java
+++ src/main/java/com/martingeo/quickstart/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.martingeo.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello Martin Geo";
+    }
 }
